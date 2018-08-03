f3231bde1f5eaa8bcb58fabd5c4824c276f73a8c
Carl Aiau <carlaiau@Carls-MBP.student.uod.otago.ac.nz>
2018-08-03 15:32:03 +1200

    Fit / Predict Working. Does not work with cross validation yet though

--- actual.py
+++ actual.py
@@ -1,12 +1,14 @@
 import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 
 
-"""
-Counts the number of label classes for an array of data.
-Because the label is in the last index of every row, this is easy
-"""
 def class_counts( data ):
+    """
+    Counts the number of label classes for an array of data.
+    Because the label is in the last index of every row, this is easy
+    """
     counts = {}
     for row in data:
         label = row[-1]
@@ -15,12 +17,11 @@ def class_counts( data ):
         counts[label] += 1
     return counts
 
-
-"""
-A Question is used to partition a dataset.
-This class records the column/feature and the value that we are comparing with
-"""
 class Question:
+    """
+    A Question is used to partition a dataset.
+    This class records the column/feature and the value that we are comparing with
+    """
     def __init__( self, feature, value ):
         self.feature = feature
         self.value = value
@@ -32,56 +33,50 @@ class Question:
         return "Is %s , >= %s?" % (
             self.feature, str(self.value))
 
-"""
-A Leaf node classifies data.
-This holds a dictionary of class (e.g., "Apple") -> number of times
-it appears in the rows from the training data that reach this leaf.
-"""
-class Leaf:
-    def __init__( self, rows ):
-        self.predictions = class_counts( rows )
+class Node:
+    """
+    A Leaf node classifies data.
+    This holds a dictionary of class (e.g., "Apple") -> number of times
+    it appears in the rows from the training data that reach this leaf.
 
-"""
-A Decision Node asks a question.
-We reference the question, the true branch,
-and the false branch. Both branchs exist, otherwise
-this Decision Node would be a
-Leaf node as there is no further branching
-"""
-class Decision_Node:
+    A Decision Node asks a question.
+    We reference the question, the true branch,
+    and the false branch. Both branchs exist, otherwise
+    this Decision Node would be a
+    Leaf node as there is no further branching
+    """
+    def __init__( self, is_decision = 0, rows = None, question = None, true_branch = None, false_branch = None):
+        if is_decision is 1:
+            self.is_decision = 1
+            self.question = question
+            self.true_branch = true_branch
+            self.false_branch = false_branch
+        else:
+            self.is_decision = 0;
+            self.predictions = class_counts( rows )
 
-    def __init__( self, question, true_branch, false_branch ):
-        self.question = question
-        self.true_branch = true_branch
-        self.false_branch = false_branch
+    def fancy_print(self):
+        if self.is_decision is 0:
+            print(self.predictions)
 
-
-
-
+    def top_pick(self):
+        if self.is_decision is 0:
+            best_class = 0;
+            best_number = 0
+            for pred_class, pred_number in self.predictions.items():
+                if pred_number > best_number:
+                    best_class = pred_class
+                    best_number = pred_number
+            return best_class
 
 class Tree:
 
-
-    """
-    Create a new 2d numpy array.
-    Append the target class, for ever array of data labels. This reduces the
-    complexity when computing the gini index, as we don't need to make reference
-    to the dataset.target array
-    """
-    def __init__ (self, dataset):
-        processed_data = np.empty( [len( dataset.data ), len( dataset.data[0] ) + 1] )
-        for i in range( len( dataset.data ) ):
-            processed_data[i] = np.append( dataset.data[i], dataset.target[i] )
-        self.data = processed_data
-        self.root_node = None
-        self.stopping_criteria = len( dataset.data) / 10 # 10% of dataset
-
-    """
-    Partitions a dataset.
-    For each row in the dataset, check if it matches the question.
-    If so, add it to true_rows otherwise add it to false_rows
-    """
     def partition( self, rows, question):
+        """
+        Partitions a dataset.
+        For each row in the dataset, check if it matches the question.
+        If so, add it to true_rows otherwise add it to false_rows
+        """
         true_rows   = []
         false_rows  = []
         for row in rows:
@@ -91,11 +86,10 @@ class Tree:
                 false_rows.append( row )
         return true_rows, false_rows
 
-    """
-    Calculate the Gini for a list of rows.
-    """
     def gini( self, rows ):
-
+        """
+        Calculate the Gini for a list of rows.
+        """
         counts = class_counts( rows )
         impurity = 1
         for label in counts:
@@ -103,13 +97,12 @@ class Tree:
             impurity -= prob_of_label**2
         return impurity
 
-
-    """
-    Information Gain.
-    The uncertainty of the starting node, minus the weighted impurity of
-    two child nodes.
-    """
     def info_gain( self, left, right, current_uncertainty):
+        """
+        Information Gain.
+        The uncertainty of the starting node, minus the weighted impurity of
+        two child nodes.
+        """
         prob_left = float( len( left ) ) / ( len( left ) + len( right ) )
         prob_right = 1 - prob_left
         info_gain = current_uncertainty;
@@ -117,11 +110,11 @@ class Tree:
         info_gain -= prob_right * self.gini ( right )
         return info_gain
 
-    """
-    Find the best question to ask by iterating over every feature / value
-    and calculating the information gain. Inefficient AF
-    """
     def find_best_split( self, rows ):
+        """
+        Find the best question to ask by iterating over every feature / value
+        and calculating the information gain. Inefficient AF
+        """
         most_gain = 0  # keep track of the best information gain
         best_question = None  # keep train of the column / Value that produced best gain
         current_gini = self.gini( rows )
@@ -149,16 +142,29 @@ class Tree:
 
         return most_gain, best_question
 
-    """
-    Start the intial building of the tree, via calling the recursive function
-    """
-    def create(self):
+    def fit(self, x, y):
+        """
+        Importing training data and setting stopping criteria
+
+        Create a new 2d numpy array.
+        Append the target class, for ever array of data labels. This reduces the
+        complexity when computing the gini index, as we don't need to make reference
+        to the y array
+
+        Start the intial building of the tree via recursive building,
+        """
+        merged_train = np.empty( [len( x ), len( x[0] ) + 1] )
+        for i in range( len( x ) ):
+            merged_train[i] = np.append( x[i], y[i] )
+        self.data = merged_train
+        self.stopping_criteria = len( self.data) / 20 # 10% of dataset
         self.root_node = self.build(self.data)
-    """
-    Builds the tree.
-    Recursive AF!
-    """
+
     def build( self, rows ):
+        """
+        Builds the tree.
+        Recursive AF!
+        """
 
         # Attempt to partition dataset on each attribute.
         # Return the best question that gives us the most information gain
@@ -167,7 +173,7 @@ class Tree:
         # Base case: no further info gain
         # we'll return a leaf, and stop the recursion.
         if gain == 0:
-            return Leaf( rows )
+            return Node(0, rows)
 
         true_rows, false_rows = self.partition( rows, question )
 
@@ -181,16 +187,39 @@ class Tree:
         # This records the best feature / value to ask at this point,
         # as well as the branches to follow
         # dependingo on the answer.
-        return Decision_Node(question, true_branch, false_branch)
+        return Node(1, None, question, true_branch, false_branch)
+
+    def predict( self, x, y):
+        n = len(x)
+        correct = 0;
+        for i in range(n):
+            if self.classify(x[i], self.root_node) == y[i]:
+                correct += 1
+        print("N: %s\tC: %s\t%.2f%%" % (n, correct, (correct/n * 100)))
+
+    def classify( self, row, node):
+
+        # Base Case: Leaf!
+        if node.is_decision is 0:
+            return node.top_pick()
+
+        # Decide whether to follow the true-branch or the false-branch.
+        # Compare the feature / value stored in the node,
+        # to the example we're considering.
+        if node.question.match(row):
+            return self.classify(row, node.true_branch)
+        else:
+            return self.classify(row, node.false_branch)
 
 
-    def start_dump( self ):
+    # Printing functions used for debuging
+    def debug_output( self ):
         self.dump( self.root_node, "" )
 
     def dump( self, node, spacing="" ):
 
         # Base Case: Leaf!
-        if isinstance(node, Leaf):
+        if node.is_decision is 0:
             print ( spacing + "Predict", node.predictions )
             return
 
@@ -206,26 +235,13 @@ class Tree:
         self.dump(node.false_branch, spacing + "\t")
 
 
-    """
-    See the 'rules of recursion' above.
-    """
-    def classify( self, row, node):
-
-
-        # Base Case: Leaf!
-        if isinstance(node, Leaf):
-            return node.predictions
-
-        # Decide whether to follow the true-branch or the false-branch.
-        # Compare the feature / value stored in the node,
-        # to the example we're considering.
-        if node.question.match(row):
-            return self.classify(row, node.true_branch)
-        else:
-            return self.classify(row, node.false_branch)
-
 if __name__ == '__main__':
-    dataset = load_iris()
-    my_tree = Tree(dataset)
-    my_tree.create()
-    my_tree.start_dump()
+    iris = load_iris()
+    for i in range(100):
+        x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
+
+        clf = Tree()
+        clf.fit(x_train, y_train)
+        clf.predict(x_test, y_test)
+    #scores = cross_val_score(my_tree, dataset.data, dataset.target, cv=5)
+    #print(scores)
